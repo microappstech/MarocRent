cd3713ff22c27ae52e7bfe6cc6e308d5d7e81c24
Hamza <Hamza@DESKTOP-0GPP13H>
2023-08-07 22:50:21 +0100

    responsive houses page

---
+++ Assets/js/Houses.js
@@ -0,0 +1,6 @@
+let IconFilter = document.querySelector(".filterIcon");
+let filterComp = document.getElementById("filter");
+IconFilter.onclick = ()=>{
+    filterComp.style.display = "block";
+    IconFilter.style.display ="none"
+}
