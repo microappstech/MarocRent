0937d4a353ad138a75c99c4dedb40eddf4d8237b
Hamza <Hamza@DESKTOP-0GPP13H>
2023-08-09 22:22:57 +0100

    add page houses and sign page house

--- Assets/js/Houses.js
+++ Assets/js/Houses.js
@@ -1,6 +1,21 @@
 let IconFilter = document.querySelector(".filterIcon");
 let filterComp = document.getElementById("filter");
+let IconClose = document.querySelector(".closeIcon");
+let filterVisible = localStorage.getItem("filterVisible");
+if(filterVisible=="visible"){
+    filterComp.style.display = "block";
+    IconFilter.style.display ="none";
+}else{
+    filterComp.style.display = "none";
+    IconFilter.style.display ="block";
+}
 IconFilter.onclick = ()=>{
     filterComp.style.display = "block";
-    IconFilter.style.display ="none"
+    IconFilter.style.display ="none";
+    localStorage.setItem('filterVisible',"visible")
+}
+IconClose.onclick =()=>{
+    filterComp.style.display = "none";
+    IconFilter.style.display ="block";
+    localStorage.setItem('filterVisible',"invisible")
 }
